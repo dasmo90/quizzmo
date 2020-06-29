619b06f7e46790b46b311f8810dab34ae8b35c3b
Moritz Bürger <moritz.buerger@prodyna.com>
2020-06-29 10:24:58 +0200

    added sample http request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'quizzmo';
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    console.log(this.title);
+    this.fetchData();
+  }
+
+  private fetchData(): void {
+    this.apiService.fetch().subscribe(
+      (data) => {
+        console.log(data);
+      }, (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +8,8 @@ import {HttpClient} from '@angular/common/http';
 export class ApiService {
 
   constructor(private httpClient: HttpClient) { }
+
+  fetch(): Observable<any> {
+    return this.httpClient.get('https://www.techiediaries.com/api/data.json');
+  }
 }
